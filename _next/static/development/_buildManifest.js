ff9932c35c18472073e5b67dd0c0745ff1fc4575
Goran <goran@bit.mk>
2022-05-23 00:42:20 +0200

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/resources/[id]":["static\u002Fchunks\u002Fpages\u002Fresources\u002F[id].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fresources\u002F[id]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
